b55ca97ab1e4f0afa2ca3239b122a56f9232e413
milosz <miloszloszek@gmail.com>
2017-10-08 20:00:59 +0200

    next_commit

---
+++ New Unity Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject singleElement;
+	public float scaleDivide;
+	public int iterations;
+	public int angle;
+
+	void Start () {
+		GenerateObjects ();
+	}
+
+	public void GenerateObjects () {
+
+		GameObject el1 = Instantiate (singleElement);
+		el1.transform.position = Vector3.one;
+
+		GameObject el2 = Instantiate (singleElement);
+		el2.transform.localScale = el1.transform.localScale * scaleDivide;
+		el2.transform.position = el1.transform.position + (new Vector3(0, ((el1.transform.lossyScale.x + el2.transform.lossyScale.x) / 2), 0));
+
+		GameObject el3 = Instantiate (singleElement);
+		el3.transform.localScale = el1.transform.localScale * scaleDivide;
+		el3.transform.position = el1.transform.position - (new Vector3(0, ((el1.transform.lossyScale.x + el3.transform.lossyScale.x) / 2), 0));
+
+		el2.transform.parent = el1.transform;
+		el3.transform.parent = el1.transform;
+	}
+}
--- New Unity Project/Assets/Scripts/SphereSizeController.cs
+++ New Unity Project/Assets/Scripts/SphereSizeController.cs
@@ -7,25 +7,53 @@ public class SphereSizeController : MonoBehaviour
 	public int index;
 	public int angle;
 	public float scaleDivide;
+	public int scaleIndex = 0;
+	public bool wasAlready;
+
 	private bool doRotate = false;
+	private GameObject child1;
+	private GameObject child2;
 
 	void Start ()
 	{
-		makeChildren ();
+		if (wasAlready){
+			wasAlready = false;
+			makeChildren (index);
+		}
 	}
 
-	void makeChildren ()
+	public void makeChildren (int index)
 	{
 		index--;
 		if (index > 0) {
-			transform.rotation *= Quaternion.Euler (0, angle, 0);
+			
+			transform.rotation *= Quaternion.Euler (angle, 0, 0);
+
 			var instance = Instantiate (gameObject);
 			instance.transform.localScale *= scaleDivide;
-			instance.transform.Translate (new Vector3 (((transform.localScale.x / 2) + (instance.transform.localScale.x / 2)), 0, 0));
-			transform.rotation *= Quaternion.Euler (0, -angle * 2, 0);
+			instance.transform.Translate (new Vector3(0, ((gameObject.transform.lossyScale.x + instance.transform.lossyScale.x) / 2), 0));
+
+
+			transform.rotation *= Quaternion.Euler (-angle * 2, 0 , 0);
+
 			var instance2 = Instantiate (gameObject);
 			instance2.transform.localScale *= scaleDivide;
-			instance2.transform.Translate (new Vector3 (((transform.localScale.x / 2) + (instance.transform.localScale.x / 2)), 0, 0));
+			instance2.transform.Translate (new Vector3(0, ((gameObject.transform.lossyScale.x + instance2.transform.lossyScale.x) / 2), 0));
+//			instance.transform.parent = gameObject.transform;
+//			instance2.transform.parent = gameObject.transform;
+
+			transform.rotation *= Quaternion.Euler (angle, 0, 0);
+//
+//			Vector3 position1 = transform.TransformPoint (instance.transform.position);
+//			Vector3 position2 = transform.TransformPoint (instance2.transform.position);
+//
+			instance.transform.parent = gameObject.transform;
+			instance2.transform.parent = gameObject.transform;
+
+			instance.GetComponent<SphereSizeController>().makeChildren (index);
+			instance2.GetComponent<SphereSizeController>().makeChildren (index);
+//			instance.transform.position = position1;
+//			instance2.transform.position = position2;
 		}
 	}
 
@@ -36,7 +64,7 @@ public class SphereSizeController : MonoBehaviour
 		}
 		if (doRotate) {
 			//transform.Rotate(Vector3.up * 10 * Time.deltaTime, Space.World);
-			transform.RotateAround (Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+			transform.RotateAround(transform.position, transform.up, 20 * Time.deltaTime);
 		}
 	}
 }
